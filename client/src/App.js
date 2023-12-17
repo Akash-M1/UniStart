a84e5c7642cb631cd2827f6bef2274706779da3a
Akash-M1 <83898316+Akash-M1@users.noreply.github.com>
2023-12-17 18:38:23 +0530

    URL Change

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
 
       <div className="mt-5 d-flex justify-content-center">
         <Card>
-          <SearchBar url="http://localhost:8000/college-data" parameter_name="college_name" onSearchResults={handleSearchResults} />
+          <SearchBar url="https://api.unistart.online/college-data" parameter_name="college_name" onSearchResults={handleSearchResults} />
         </Card> 
       </div>
 
